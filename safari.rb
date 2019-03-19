d5221d562830386e04ce8cce56e69bde1805ffda
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-03-18 21:58:48 -0400

    got the data to display. working on incrementing the animals seen

--- safari.rb
+++ safari.rb
@@ -8,9 +8,38 @@ end
 
 require "active_record"
 
-ActiveRecord::Base.logger = Longer.new(STDOUT)
+ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 ActiveRecord::Base.establish_connection(
   adapter: "postgresql",
-  database: "safari_vacation"
+  database: "safari_vacation",
 )
+
+class Animal < ActiveRecord::Base
+end
+
+# display all animals when type display_all in the terminal
+
+puts "Welcome to the safari adventure! Take a look at all the animals seen!"
+display_all = gets.chomp
+animals = Animal.all
+p animals
+
+puts "How many lions did you see?"
+increment_animals_seen = gets.chomp
+p increment_animals_seen
+animals_seen = Animal.find_by({"lion" => increment_animals_seen})
+
+
+
+if increment_animals_seen.empty?
+  puts "There was no animal seen"
+else 
+  puts "Saw animals!"
+
+  animals_seen.each do |lion|
+    puts "Just saw #{lion.seen_count} lions!"
+    lion.save
+    p lions
+  end
+end
