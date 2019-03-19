0310ca0525c323cce8c316da8d3a2ad222799654
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-03-19 15:35:36 -0400

    added extint feature

--- safari.rb
+++ safari.rb
@@ -20,31 +20,62 @@ end
 
 # display all animals when type display_all in the terminal
 
+loop do
 puts "Welcome to the safari adventure! Take a look at all the animals seen!"
-display_all = gets.chomp
-animals = Animal.all
-p animals
+comment = gets.chomp
+if comment == "display_all"
+  p Animal.all
+  break
+elsif comment == "increment"
+    puts "What kind of animal did you see?"
+    increment_animals_seen = gets.chomp
+    animal = Animal.find_by({"species" => increment_animals_seen})
+    animal.seen_count += 1
+    animal.save
+    puts "#{animal.species} count: #{animal.seen_count}"
+elsif comment == "species"
+  puts "What was the species you saw?"
+  species_seen = gets.chomp
+  # puts "where did you see #{species_seen}"
+  animal = Animal.find_by({"species" => species_seen})
+  p "Saw #{animal.seen_count} #{animal.species}s #{animal.last_seen_location}"
+elsif comment == "spot"
+  puts "What animal did you spot?!"
+  animal_seen = gets.chomp
+  puts "Where did you see #{animal_seen}?"
+  location = gets.chomp
+  animal = Animal.find_by({"species" => animal_seen})
+  animal.last_seen_location = location
+  animal.save
+  break
+elsif comment == "extinct"
+  puts "What's a location you wanna destroy?"
+  extinct_location = gets.chomp
+  animal = Animal.where({"last_seen_location" => extinct_location})
+  animal.destroy_all
+  p "#{extinct_location} destroyed!"
+elsif comment == "total"
+  p Animal.sum(:seen_count)
+  end
+end
+# 5.times{puts}
 
-5.times{puts}
 
-puts "What kind of animal did you see?"
-increment_animals_seen = gets.chomp
-lions_seen = Animal.find_by(species: "lion")
-p lions_seen.seen_count
+# lions_seen = Animal.find_by(species: "lion")
+# p lions_seen.seen_count
 # p lions_seen_count += Animal.seen_count
-animals_seen = Animal.where({"species" => increment_animals_seen})
+# animals_seen = Animal.where({"species" => increment_animals_seen})
 # animals_seen.update({"seen_count" => increment_animals_seen})
-animal_seen = Animal.find_by({"species" => increment_animals_seen})
 # animals.seen_count += animal_seen
 
 
-if increment_animals_seen.empty?
-  puts "There was no animal seen"
-else 
-  puts animals_seen
+# if increment_animals_seen.empty?
+#   puts "There was no animal seen"
+# else 
+#   puts animals_seen
 
   # animals_seen.each do |one_animal|
   #   puts "Just saw #{one_animal}!"
   #   animals_seen.save
   # end
-end
+# end
